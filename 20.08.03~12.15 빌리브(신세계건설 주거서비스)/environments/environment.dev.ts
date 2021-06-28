afea8b3c195826227dc8eb1d90dc8247fc087b7b
sadwingxd <sadwingxd@gmail.com>
2021-06-28 16:21:23 +0900

    commit

--- 20.08.03~12.15 빌리브(신세계건설 주거서비스)/environments/environment.dev.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: false,
-  development: true,
-  local: false,
-  pagePath: './pages',
-  hostname: '',
-  baseDomain: 'http://d_service.shinsegae-con.co.kr',
-  curenv: 'www-dev'
-};
--- 20.08.03~12.15 빌리브(신세계건설 주거서비스)/environments/environment.device-prod.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: false,
-  development: true,
-  local: false,
-  pagePath: './pages',
-  hostname: 'http://service.shinsegae-con.co.kr',
-  baseDomain: 'http://service.shinsegae-con.co.kr',
-  curenv: 'device-prod'
-};
