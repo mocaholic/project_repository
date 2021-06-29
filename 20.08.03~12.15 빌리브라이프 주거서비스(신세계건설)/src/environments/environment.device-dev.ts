9930af0566cc069154b40abb69b657a2e134f72c
sadwingxd <sadwingxd@gmail.com>
2021-06-29 17:47:42 +0900

    수정

--- 20.08.03~12.15 빌리브라이프 주거서비스(신세계건설)/src/environments/environment.device-dev.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: false,
-  development: true,
-  local: false,
-  pagePath: './pages',
-  hostname: 'http://d_service.shinsegae-con.co.kr',
-  baseDomain: 'http://d_service.shinsegae-con.co.kr',
-  curenv: 'device-dev'
-};
--- 20.08.03~12.15 빌리브라이프 주거서비스(신세계건설)/src/environments/environment.device-windfall.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: false,
-  development: true,
-  local: false,
-  pagePath: './pages',
-  hostname: 'http://villiv-front.wdfall.com:8008',
-  baseDomain: 'http://villiv-front.wdfall.com:8008',
-  curenv: 'device-dev'
-};
